a764d7d009c47fb40bf9de98bd3f5de8daf1e93a
doyeon <1162004@naver.com>
2022-11-28 10:49:25 +0900

    COMMIT

--- Project_Sample_JPA(Spring Boot)/shop/src/main/java/com/shop/controller/MemberController.java
+++ Project_Sample_JPA(Spring Boot)/shop/src/main/java/com/shop/controller/MemberController.java
@@ -17,6 +17,7 @@ public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
+    // 회원가입을 위한 경로
     @GetMapping("/new")
     public String memberForm(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
@@ -28,5 +29,16 @@ public class MemberController {
         Member member = Member.createMember(memberFormDto, passwordEncoder); // 실제로 값을 저장할 곳을 만듦
         memberService.saveMember(member);
         return "redirect:/";
+    }
+
+    // 로그인을 위한 경로
+    @GetMapping("/login")
+    public String loginMember() {
+        return "/member/memberLoginForm";
+    }
+    @GetMapping("/login/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
+        return "member/memberLoginForm";
     }
 }
--- Project_Sample_JPA(Spring Boot)/shop/src/main/java/com/shop/service/MemberService.java
+++ Project_Sample_JPA(Spring Boot)/shop/src/main/java/com/shop/service/MemberService.java
@@ -2,13 +2,18 @@ package com.shop.service;
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 @Service
 @Transactional // 여러 절차의 일을 묶어서 처리
 @RequiredArgsConstructor // 이게 없으면 생성자를 따로 만들어서 의존성 주입을 받아야함
-public class MemberService {
+// UserDetailsService : DB에서 회원 정보를 가져오는 기능을 수행(loadUserByUserName 메소드를 가지고 있음)
+public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository; // RequiredArgsConstructor가 있으면 이렇게 한줄로 끝날 수 있음 // final 꼭 붙여야 의존성 주입 가능
 
     //  but, RequiredArgsConstructor가 없으면 이렇게 해야함
@@ -27,5 +32,18 @@ public class MemberService {
         if(findMember != null) { // findMember가 NOT NULL이면 이미 가입된 회원 => 에러처리
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+    @Override // 회원정보를 조회하여 사용자의 정보와 권한을 갖는 UserDetail 인터페이스 반환
+    // UserDetails : 스프링 시큐리티에서 회원 정보를 담기 위해서 사용하는 인터페이스
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+        if(member == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
